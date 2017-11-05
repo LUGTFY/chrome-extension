e51bbabf71bfbb1cce0571086f724140af2e2a5c
sw-yx <swyx@swyx.io>
2017-11-05 03:16:08 -0500

    add

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var newDiv = document.createElement("a"); 
-var newContent = document.createTextNode(" | Can't find what you want? Let Us Google That For You"); 
-var searchStr = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent('q').replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));  
-newDiv.setAttribute('href', "http://youtube.com/?q=" + searchStr)
-newDiv.setAttribute('target', "_blank")
-newDiv.appendChild(newContent)
-document.getElementById('resultStats').appendChild(newDiv)
+var newDiv = document.createElement("a");
+var newContent = document.createTextNode(" | Can't find? Send to LUGTFY 🔗");
+// var searchStr = decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent('q').replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+var searchStr = encodeURIComponent(document.getElementById("lst-ib").value);
+newDiv.setAttribute(
+  "href",
+  "https://letusgogglethatforyou.firebaseapp.com/submit/" + searchStr
+);
+newDiv.setAttribute("target", "_blank");
+newDiv.appendChild(newContent);
+document.getElementById("resultStats").appendChild(newDiv);
